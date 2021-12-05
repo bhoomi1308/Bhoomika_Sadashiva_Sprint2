dcf572f1bea7b2585598a377a3f804ac23d3c8ff
Bhoomika S <bhoomi3239@gmail.com>
2021-12-05 17:46:41 +0530

    Add files via upload

---
+++ UserTable.sql
@@ -0,0 +1,14 @@
+create table User(
+UserId integer,
+FirstName varchar2(50),
+LastName varchar2(50),
+EmailId varchar2(15),
+Password varchar(25)
+);
+
+insert into User(UserId,FirstName,LastName,EmailId,Password) values (1,'Bhoomika','Sadashiva','bhoomika.sadashiva@socgen.com','1234');
+insert into User(UserId,FirstName,LastName,EmailId,Password) values (2,'Bhoomika','S','bhoomika@gmail.com','b@1234');
+
+
+
+
